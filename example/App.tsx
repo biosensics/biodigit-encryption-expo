2b4df5876f3e7739f9573d5a33122456808b0a16
Aidan Daly <74743624+dalyaidan1@users.noreply.github.com>
2024-11-15 10:33:54 -0500

    strip it down

--- example/App.tsx
+++ example/App.tsx
@@ -1,73 +1,10 @@
-import { useEvent } from 'expo';
-import BioDigitEncryptionExpo, { BioDigitEncryptionExpoView } from 'biodigit-encryption-expo';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import * as BioDigitEncryption from 'biodigit-encryption-expo';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  const onChangePayload = useEvent(BioDigitEncryptionExpo, 'onChange');
-
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}>
-        <Text style={styles.header}>Module API Example</Text>
-        <Group name="Constants">
-          <Text>{BioDigitEncryptionExpo.PI}</Text>
-        </Group>
-        <Group name="Functions">
-          <Text>{BioDigitEncryptionExpo.hello()}</Text>
-        </Group>
-        <Group name="Async functions">
-          <Button
-            title="Set value"
-            onPress={async () => {
-              await BioDigitEncryptionExpo.setValueAsync('Hello from JS!');
-            }}
-          />
-        </Group>
-        <Group name="Events">
-          <Text>{onChangePayload?.value}</Text>
-        </Group>
-        <Group name="Views">
-          <BioDigitEncryptionExpoView
-            url="https://www.example.com"
-            onLoad={({ nativeEvent: { url } }) => console.log(`Loaded: ${url}`)}
-            style={styles.view}
-          />
-        </Group>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-function Group(props: { name: string; children: React.ReactNode }) {
-  return (
-    <View style={styles.group}>
-      <Text style={styles.groupHeader}>{props.name}</Text>
-      {props.children}
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Theme: {BioDigitEncryption.getTheme()}</Text>
     </View>
   );
 }
-
-const styles = {
-  header: {
-    fontSize: 30,
-    margin: 20,
-  },
-  groupHeader: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  group: {
-    margin: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-  },
-  view: {
-    flex: 1,
-    height: 200,
-  },
-};
--- src/BioDigitEncryptionExpoModule.ts
+++ src/BioDigitEncryptionExpoModule.ts
@@ -1,11 +1,6 @@
 import { NativeModule, requireNativeModule } from 'expo';
 
-import { BioDigitEncryptionExpoModuleEvents } from './BioDigitEncryptionExpo.types';
-
-declare class BioDigitEncryptionExpoModule extends NativeModule<BioDigitEncryptionExpoModuleEvents> {
-  PI: number;
-  hello(): string;
-  setValueAsync(value: string): Promise<void>;
+declare class BioDigitEncryptionExpoModule extends NativeModule {
 }
 
 // This call loads the native module object from the JSI.

a9a1353750aa52f82269f2758e1b107096510087
Aidan Daly <74743624+dalyaidan1@users.noreply.github.com>
2024-11-15 17:00:40 -0500

    basics RN side

--- example/metro.config.js
+++ example/metro.config.js
@@ -31,4 +31,18 @@ config.transformer.getTransformOptions = async () => ({
   },
 });
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'crypto') {
+    // when importing crypto, resolve to react-native-quick-crypto
+    return context.resolveRequest(
+      context,
+      'react-native-quick-crypto',
+      platform,
+    );
+  }
+  // otherwise chain to the standard Metro resolver.
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+
 module.exports = config;

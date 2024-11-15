a2a797513e84a470893ac9d93fdb025334b452b9
Aidan Daly <74743624+dalyaidan1@users.noreply.github.com>
2024-11-15 15:59:03 -0500

    draft kotlin side

--- android/src/main/java/com/biosensics/biodigitencryptionexpo/BioDigitEncryptionExpoModule.kt
+++ android/src/main/java/com/biosensics/biodigitencryptionexpo/BioDigitEncryptionExpoModule.kt
@@ -3,13 +3,40 @@ package com.biosensics.biodigitencryptionexpo
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import expo.modules.kotlin.Promise
+import expo.modules.kotlin.exception.toCodedException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+import java.util.UUID
 
 class BioDigitEncryptionExpoModule : Module() {
   override fun definition() = ModuleDefinition {
     Name("BioDigitEncryptionExpo")
 
+
     AsyncFunction("encrypt") { session: String, path: String, promise: Promise ->
-      promise.resolve("stub")
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                // get aq byte array from the session
+                val sessionByteArray = session.toByteArray()
+
+                // make a new SymKey instance
+                val key = SymKey(sessionByteArray)
+
+                val fileName = path.substringAfterLast("/")
+
+                // get the file path as a byte array
+                val fileStream = File(path).readBytes()
+
+                // encrypt the path
+                val encrypted = encryptFile(fileStream, fileName, UUID.randomUUID().toString(), key)
+
+                promise.resolve(encrypted)
+            } catch (e: Exception) {
+                promise.reject(e.toCodedException())
+            }
+        }
     }
   }
 }
